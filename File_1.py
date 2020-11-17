300cd6f21c7d445d6daa9f1555663efb1e82a108
Shoumik-20 <55137590+Shoumik-20@users.noreply.github.com>
2020-11-17 11:00:36 +0530

    Added Neural Network

--- File_1.py
+++ File_1.py
@@ -6,13 +6,29 @@ from torch.autograd import Variable
 import pandas as pd
 import numpy as np
 
+class Net(nn.Module):
+    def __init__(self):
+        super(Net,self).__init__()       # fc stands for fully connected
+        self.fc1 = nn.Linear(4,10)
+        self.fc2 = nn.Linear(10,10)
+        self.fc3 = nn.Linear(10,3)
+        self.softmax = nn.Softmax(dim = 1)
+
+    def forward(self, X):
+        X = F.relu(self.fc1(X))
+        X = self.fc2(X)
+        X = self.fc3(X)
+        X = self.softmax(X)
+
+        return X
+
 iris = datasets.load_iris()
-#iris_df = pd.DataFrame(iris['data'], columns=iris['feature_names'])
 
-#print(iris_df)
-
-df= pd.DataFrame(data= np.c_[iris['data'], iris['target']],
+iris_df= pd.DataFrame(data= np.c_[iris['data'], iris['target']],
                  columns= iris['feature_names'] + ['target'])
 
-df['species'] = pd.Categorical.from_codes(iris.target, iris.target_names)
-print(df)
+iris_df['species'] = pd.Categorical.from_codes(iris.target, iris.target_names)
+print(iris_df)
+
+net = Net()
+print(net)
